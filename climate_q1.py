ea417fcbaa9a596032e6531382278fd87f73929f
Anton Koulakov <anton.koulakov@griffithuni.edu.au>
2023-09-14 10:40:46 +1000

    Added the code that retrieves all of the required values from the database.

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+database = sqlite3.connect("./climate.db")
+cursor = database.cursor()
+
 years = []
 co2 = []
 temp = []
+
+cursor.execute("SELECT * FROM ClimateData")
+data = cursor.fetchall()
+
+for x in data:
+    years.append(x[0])
+    co2.append(x[1])
+    temp.append(x[2])
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -16,3 +28,5 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
+
+database.close()
